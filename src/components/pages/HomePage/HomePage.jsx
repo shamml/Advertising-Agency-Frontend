c6c6249a2c28c4a84600e844d13c926bd579fae6
sham <shamil.gachaev@gmail.com>
2022-03-22 12:46:38 +0300

    Добавил компонент Сити-Формат

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './HomePage.module.css';
 import Carousel from '../../carousel/Carousel';
 import VisitCard from '../../visitCard/VisitCard';
+import StFormat from '../../STFormat/STFormat'
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <Carousel />
       <VisitCard />
+      <StFormat/>
     </div>
   );
 };
