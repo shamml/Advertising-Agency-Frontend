77a7a5776b7c697069e3c74624667096a9da69e5
username <nomadicalli@mail.ru>
2022-03-22 13:00:22 +0300

    Добавил компонент Map

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Map.module.css';
+
+const Map = () => {
+  return (
+    <div className={styles.map}>
+      <div className={styles.mapImage}>
+        <img src="" alt="no img" />
+      </div>
+      <div className={styles.iframe}>
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3Ae6df1e7fb92f7725f4fd1d1e07fdeeadec74a6a9682fd685998d5f8896a28e33&amp;source=constructor"
+          width="961"
+          height="400"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './HomePage.module.css';
 import Carousel from '../../carousel/Carousel';
 import VisitCard from '../../visitCard/VisitCard';
+import Map from '../../map/Map';
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
       <Carousel />
       <VisitCard />
+      <Map />
     </div>
   );
 };
