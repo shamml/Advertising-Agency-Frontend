02d6d2d52824e762e9e60d7c9dd709371bcca065
rakhmaev <rakhmaev1996@mail.ru>
2022-03-22 12:54:42 +0300

    Внес правки

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,19 +3,22 @@ import styles from "./Carousel.module.css"
 
 function Carousel(props) {
   return (
-    <div className={styles.carouselBlock}>
+    <div className={styles.carouselDark}>
+      <div className={styles.carouselBlock}>
         <div className={styles.carouselContent}>
           <div className={styles.title}>
             РЕКЛАМНОЕ <br /> АГЕНТСТВО <br /> ПОЛНОГО <br /> ЦИКЛА
           </div>
           <div className={styles.form}>
             <div className={styles.formTitle}>Получить бесплатную консультацию</div>
-            <input type="text" />
-            <input type="text" />
+            <input className={styles.formFirstInput} type="text" placeholder="Ваше имя..." />
+            <input className={styles.formSecondInput} type="text" placeholder="Ваше телефон..." />
             <button className={styles.formBtn}>Отправить</button>
           </div>
         </div>
       </div>
+    </div>
+    
   );
 }
 
