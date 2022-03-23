c7e0cf85e5333f20d79916b9c9d2c126492ba38d
rakhmaev <rakhmaev1996@mail.ru>
2022-03-23 22:14:12 +0300

    Внес правки

---
+++ src/redux/features/visitcard.js
@@ -0,0 +1,34 @@
+const initialState = {
+  loading: false,
+  visitCards: [],
+  error: null,
+};
+
+export default function visitCards(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export const addVisitCard = (paper, count, delivery) => {
+  return async (dispatch, ) => {
+    dispatch({ type: "visit/add/pending"});
+
+    const res = fetch("http://localhost:3030/visitcards", {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ typePaper: paper, count: count, delivery: delivery }), 
+    })
+
+    const visitCard = res.json();
+
+    if (visitCard.error) {
+      dispatch({ type: "visit/add/rejected", error: visitCard.error});
+    } else {
+      dispatch({ type: "visit/add/fulfilled", payload: visitCard});
+    }
+  }
+}
